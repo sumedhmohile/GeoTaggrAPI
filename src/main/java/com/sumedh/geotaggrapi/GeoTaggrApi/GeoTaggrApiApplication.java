97d879fd31a8977a6e3dc8bd1d39311c19752dd1
sumedh <summm02@gmail.com>
2021-04-17 15:46:55 +0530

    Initial commit

---
+++ src/main/java/com/sumedh/geotaggrapi/GeoTaggrApi/GeoTaggrApiApplication.java
@@ -0,0 +1,13 @@
+package com.sumedh.geotaggrapi.GeoTaggrApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoTaggrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoTaggrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumedh/geotaggrapi/GeoTaggrApi/GeoTaggrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumedh.geotaggrapi.GeoTaggrApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoTaggrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
